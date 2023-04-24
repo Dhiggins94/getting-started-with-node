048db2c82bb9dab09729959b716e84742cef2e40
Divine Higgins <dhiggins94@gmail.com>
2023-04-24 17:36:06 -0400

    qoddi testing

--- server.js
+++ server.js
@@ -1,53 +1,18 @@
-"use strict";
 
-const express = require("express");
-const path = require("path");
-const PORT = process.env.PORT || 3001;
-const HOST = process.env.HOST;
+ 
+
+'use strict';
+
+const express = require('express');
+const path = require('path')
+const PORT = process.env.PORT || 8080
+const HOST = process.env.HOST
 
 // App
-
-const express = require("express");
 const app = express();
-const http = require("http");
-const cors = require("cors");
-const { Server } = require("socket.io");
-app.use(cors());
-
-const server = http.createServer(app);
-const io = new Server(server, {
-  cors: {
-    origin: "http://localhost:3000",
-    methods: ["GET", "POST"],
-  },
-});
-
-io.on("connection", (socket) => {
-  console.log(`User Connected: ${socket.id}`);
-
-  socket.on("join_room", (data) => {
-    socket.join(data);
-    console.log(`User with ID: ${socket.id} joined room: ${data}`);
-  });
-
-  socket.on("send_message", (data) => {
-    socket.to(data.room).emit("receive_message", data);
-  });
-
-  socket.on("disconnect", () => {
-    console.log("User Disconnected", socket.id);
-  });
-});
-
-server.listen(3001, () => {
-  console.log("SERVER RUNNING");
-});
-
-app.get("/", (req, res) => {
-  res.send("Hello World - NodeJS is running on Qoddi!");
+app.get('/', (req, res) => {
+  res.send('Hello World - NodeJS is running on Qoddi!');
 });
 
 app.listen(PORT, HOST);
 console.log(`Running on http://${HOST}:${PORT}`);
-
-
